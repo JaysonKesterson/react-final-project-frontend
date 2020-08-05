715452a2cf4bc0b2a94bdb4f16cfc3157bbee9a2
jayson_kesterson <your_email@example.com>
2020-08-05 11:46:09 -0400

    adding test fetch and removing unneeded code from render function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
+export default class App extends Component{
 
   componentDidMount() {
     fetch("http://localhost:3000/api/v1/users").then(response => response.json()).then(console.log)
@@ -11,23 +10,8 @@ class App extends Component{
 
   render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );}
+    "hello"
+  );
 }
 
-export default App;
+}
