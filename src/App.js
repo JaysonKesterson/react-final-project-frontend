1c41eabcc0c23da393038de83eb7a63aea6119a8
jayson_kesterson <your_email@example.com>
2020-08-09 11:03:16 -0400

    adding routes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import {Component} from 'react';
 import './App.css';
 import { connect } from "react-redux";
 import { getCurrentUser } from "./actions/currentUser.js";
-import NavBar from "./components/NavBar.js"
+import NavBar from "./components/NavBar.js";
 import AppContainer from './components/AppContainer';
-import UserProfile from './components/UserProfile'
+import UserProfile from './components/UserProfile';
+import Login from "./components/Login.js"
+import Signup from "./components/Signup.js"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 class App extends Component{
 
@@ -15,11 +19,15 @@ class App extends Component{
 
   render() {
   return (
+    <Router>
     <div className="App">
     <NavBar/>
-    <UserProfile/>
     <AppContainer/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/signup' component={Signup}/>
+      <Route exact path='/profile' component={UserProfile}/>
     </div>
+    </Router>
   );
 }
 
