30ac822991ae617551fcf12826141940e371ff64
jayson_kesterson <your_email@example.com>
2020-08-12 09:24:40 -0400

    adding case statement to handle an addstore action

--- src/reducers/stores.js
+++ src/reducers/stores.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch(action.type) {
         case "SET_STORES":
             return action.stores
+        case "ADD_STORE":
+            return state.concat(action.store)
         default:
             return state
     }
