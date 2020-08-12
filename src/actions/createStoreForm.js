e2e87c95427c19c31ce8963249ec851c329dbc04
jayson_kesterson <your_email@example.com>
2020-08-12 09:24:05 -0400

    finishing action to create a store

--- src/actions/createStoreForm.js
+++ src/actions/createStoreForm.js
@@ -4,3 +4,46 @@ export const updateCreateStoreForm = (name, value) => {
         formData: {name,value}
     }
 }
+
+export const clearCreateStoreForm = () => {
+    return {
+        type:"CLEAR_NEW_STORE_FORM",
+    }
+}
+
+export const addStore = store => {
+    return {
+        type:"ADD_STORE",
+        store
+    }
+}
+
+export const createStore = (name, userId, history) => {
+    return dispatch => {
+        const nameToSend = {
+            store: {
+            name: name,
+            user_id: userId
+            }
+        }
+        return fetch('http://localhost:3000/api/v1/stores', {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(nameToSend)
+        })
+            .then(response => response.json())
+            .then(store => {
+                if (store.errors) {
+                    alert(store.errors)
+                } else {
+                dispatch(addStore(store.data))
+                dispatch(clearCreateStoreForm())
+                history.push(`/stores/${store.data.id}`)
+                }
+            }) 
+    }
+}
+
