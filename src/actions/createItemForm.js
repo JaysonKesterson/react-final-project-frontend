ba891edc66d7627bc659498afe5cc3243d2cd341
jayson_kesterson <your_email@example.com>
2020-08-13 13:06:14 -0400

    adding actions around creating an item

---
+++ src/actions/createItemForm.js
@@ -0,0 +1,51 @@
+export const updateCreateItemForm = (name, value) => {
+    return {
+        type:"UPDATE_NEW_ITEM_FORM",
+        formData: {name,value}
+    }
+}
+
+export const clearCreateItemForm = () => {
+    return {
+        type:"CLEAR_NEW_ITEM_FORM",
+    }
+}
+
+export const addItem = item => {
+    return {
+        type:"ADD_ITEM",
+        item
+    }
+}
+
+export const createItem = (storeId, price, name, description, condition, history) => {
+    return dispatch => {
+        const dataToSend = {
+            item: {
+            store_id: storeId,
+            price: price,
+            name: name,
+            description: description,
+            condition: condition
+            }
+        }
+        return fetch('http://localhost:3000/api/v1/items', {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dataToSend)
+        })
+            .then(response => response.json())
+            .then(item => {
+                if (item.errors) {
+                    alert(item.errors)
+                } else {
+                dispatch(addItem(item.data))
+                dispatch(clearCreateItemForm())
+                history.push(`/profile`)
+                }
+            }) 
+    }
+}
