2ead0cc73c780812298c7fc1c90d21d88925ae80
jayson_kesterson <your_email@example.com>
2020-08-13 13:12:10 -0400

    getting current users store on app load

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ import UserProfile from './components/UserProfile';
 import Login from "./components/Login.js"
 import Signup from "./components/Signup.js"
 import Home from "./components/Home.js"
-import createStoreForm from "./components/createStoreForm.js"
+import createStoreForm from "./components/CreateStoreForm.js"
+import EditStoreForm from "./components/EditStoreForm.js"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Store from './components/Store.js';
+import Store from './components/Store.js'
+import Item from './components/Item.js'
 import Stores from './components/Stores.js'
+import createItemForm from './components/CreateItemForm.js'
+import { getCurrentUserStore } from './actions/currentUser.js'
+import EditItemForm from './components/EditItemForm.js'
+
 
 
 class App extends Component{
@@ -21,13 +27,19 @@ class App extends Component{
     this.props.getCurrentUser()
   }
 
+  componentDidUpdate() {
+    if (this.props.loggedIn) { this.props.getCurrentUserStore(this.props.currentUser.id) }
+
+  }
+
   render() {
-    const { loggedIn, stores } = this.props
+    const { loggedIn, stores, items } = this.props
   return (
     <Router>
     <div className="App">
     { loggedIn ? <NavBar/> : <Home/>}
     <AppContainer/>
+      <Route exact path='/stores/:id/items/new' component={createItemForm}/>
       <Route exact path='/login' component={Login}/>
       <Route exact path='/signup' component={Signup}/>
       <Route exact path='/profile' component={UserProfile}/>
@@ -37,9 +49,17 @@ class App extends Component{
         const store = stores.find(store => store.id === props.match.params.id)
         return <Store store={store} {...props}/>
       }}/>
+      <Route exact path='/items/:id' render={props => {
+        const item = items.find(item => item.id === props.match.params.id)
+        return <Item item={item} {...props}/>
+      }}/>
       <Route exact path='/stores/:id/edit' render={props => {
         const store = stores.find(store => store.id === props.match.params.id)
-        return <createStoreForm store={store} {...props}/>
+        return <EditStoreForm store={store} {...props}/>
+      }}/>
+      <Route exact path='/items/:id/edit' render={props => {
+        const item = items.find(item => item.id === props.match.params.id)
+        return <EditItemForm item={item} {...props}/>
       }}/>
     </div>
     </Router>
@@ -51,8 +71,10 @@ class App extends Component{
 const mapStateToProps = state => {
   return ({
     loggedIn: !!state.currentUser,
-    stores: state.stores
+    stores: state.stores,
+    currentUser: state.currentUser,
+    items: state.items
   })
 }
 
-export default connect(mapStateToProps, {getCurrentUser})(App)
+export default connect(mapStateToProps, {getCurrentUser, getCurrentUserStore})(App)
