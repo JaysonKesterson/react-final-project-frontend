1a98630ee253d5c3626e6adf72318ff0e2c0e57e
jayson_kesterson <your_email@example.com>
2020-08-13 20:32:57 -0400

    changing navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,10 @@ import { NavLink } from 'react-router-dom'
 const NavBar = ( {currentUser, loggedIn} ) => {
 
     return(
-        <div className="NavBar">
-          <NavLink exact to="/stores">All Stores  |  </NavLink>
-          <NavLink exact to="/stores/new">Create Store  |  </NavLink>
-          <NavLink exact to="/profile">Profile  |  </NavLink>
+        <div className="App-header">
+          <NavLink exact to="/stores">All Stores</NavLink>
+          <NavLink exact to="/stores/new">Create Store</NavLink>
+          <NavLink exact to="/profile">Profile</NavLink>
           { loggedIn ? <Logout/> : null}
         </div>
     )
