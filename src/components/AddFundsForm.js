0ba64cb6245e811989e0cb53aa6a93dd388caed4
jayson_kesterson <your_email@example.com>
2020-08-13 20:30:38 -0400

    adding form so users can add funds to their balance

---
+++ src/components/AddFundsForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import { updateCurrentUserBalance, updateAddFundsForm } from '../actions/currentUser.js'
+
+
+
+const AddFundsForm = (props) => {
+
+    const handleChange = event => {
+        const { name, value } = event.target
+        props.updateAddFundsForm(name,value)
+    }
+
+    const handleOnSubmit = event => {
+        event.preventDefault()
+        props.updateCurrentUserBalance(props.currentUser.id, props.deposit, props.history)
+    }
+
+    return (
+        <form onSubmit={handleOnSubmit}>
+            <input type="number" min="1" max="10000" name="deposit" onChange={handleChange} value={props.deposit} placeholder={"Amount"}/>
+            <input type="submit" value="Add Funds"/>
+        </form>
+    )};
+
+    const mapStateToProps = state => {
+        return{
+            deposit: state.addFundsForm.deposit,
+            currentUser: state.currentUser
+        }
+    }
+
+export default connect(mapStateToProps, { updateCurrentUserBalance, updateAddFundsForm })(AddFundsForm);
