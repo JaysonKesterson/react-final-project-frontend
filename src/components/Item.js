ec4e294a0e58f846a54aff0fac343b8400ada33e
jayson_kesterson <your_email@example.com>
2020-08-13 20:34:08 -0400

    making links look like  buttons

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { destroyItem } from '../actions/editItemForm.js'
+import { completeTransaction } from '../actions/transaction.js'
 
-const Item = ({ item, currentUser, loggedIn, history, destroyItem, stores }) => {
+const Item = ({ item, currentUser, loggedIn, history, destroyItem, stores, completeTransaction }) => {
 
     const store = stores.find(store => store.attributes.user_id === parseInt(currentUser.id))
 
     const handleButtonClick = () => {
         destroyItem(item.id, history)
       }
+
+    const handleTransaction = () => {
+        const storeBoughtFrom = stores.find(store => item.attributes.store_id === parseInt(store.id))
+        completeTransaction(storeBoughtFrom.attributes.user_id, item.attributes.price, item.id, history)
+    }
 
     return (
     item ?
@@ -17,7 +23,7 @@ const Item = ({ item, currentUser, loggedIn, history, destroyItem, stores }) => 
     <h2>Price of Item: ${item.attributes.price}</h2>
     <h2>Description: {item.attributes.description}</h2>
     <h2>Condition: {item.attributes.condition}</h2>
-    {loggedIn && item.attributes.store_id === parseInt(store.id) ? <button onClick={handleButtonClick}>Delete Item</button> : <button>Purchase Item</button>}
+    {loggedIn && item.attributes.store_id === parseInt(store.id) ? <button onClick={handleButtonClick}>Delete Item</button> : <button onClick={handleTransaction}>Purchase Item</button>}
     </div> :
     ""
     )
@@ -33,4 +39,4 @@ const mapStateToProps = state => {
 
 
 
-export default connect(mapStateToProps, { destroyItem })(Item)
+export default connect(mapStateToProps, { destroyItem, completeTransaction })(Item)
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,19 +4,16 @@ import { Link } from 'react-router-dom'
 
 class UserProfile extends React.Component {
 
-
     render() {
       const { currentUser, store } = this.props
     return(
       <div className="UserProfile">
             <h2>Welcome {currentUser ? currentUser.attributes.name : "User"}</h2>
-            <h2>Your Balance: {currentUser ? `$${currentUser.attributes.balance}`  : "User"}</h2>
-           { store ? `My Store: ${store.attributes.name}` : "" }
+            <h2>Your Balance: {currentUser ? `$${currentUser.attributes.balance}`  : "User"} <Link className="button"to={`/users/${currentUser.id}/addFunds`}>Add Funds</Link> </h2> 
+           { store ? <h2>{store.attributes.name} {store ? <Link className="button"to={`/stores/${store.id}/edit`}>Edit Store</Link> : null} </h2> : "" }
            <br></br>
-           { store ? store.attributes.items.map( item => (<p key={item.id}>Name: {item.name} | Price: ${item.price} | Condition: {item.condition} | <Link to={`/items/${item.id}`}>Detailed Item View</Link> | <Link to={`/items/${item.id}/edit`}>Edit Item</Link> </p>)) : "" }
-        {store ? <Link to={`/stores/${store.id}/items/new`}>Add Item</Link> : null}
-        <br></br>
-        {store ? <Link to={`/stores/${store.id}/edit`}>Edit Store</Link> : null}
+           { store ? store.attributes.items.map( item => (<li key={item.id}>Name: {item.name} | Price: ${item.price} | Condition: {item.condition} | <Link className="button" to={`/items/${item.id}`}>Detailed Item View</Link> | <Link className="button" to={`/items/${item.id}/edit`}>Edit Item</Link> </li>)) : "" }
+        {store ? <h2><Link className="button-center" to={`/stores/${store.id}/items/new`}>Add Item</Link></h2> : null}
         </div>
     )
     
