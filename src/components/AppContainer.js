6efa44a34e715e379de8e158b6522710dc19b231
jayson_kesterson <your_email@example.com>
2020-08-13 20:34:53 -0400

    making app container a home page

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -4,7 +4,9 @@ import React from 'react'
 const AppContainer = () => {
     return (
         <div className="AppContainer">
-            
+            <h2 className="welcome">Welcome to Bargain Hunter!</h2>
+            <h3>Create stores and add items to them:</h3> <br></br>
+            <h5>Each User on Bargain Hunter is an entrepreneur. </h5>
         </div>
     )
 }
