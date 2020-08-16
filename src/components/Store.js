b5e484434322ccb2116c6da5e0ba3545cdfd7424
jayson_kesterson <your_email@example.com>
2020-08-16 09:50:18 -0400

    modifying store to show correct updated state

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Store = ({ store }) => {
+const Store = ({ store, items }) => {
     return (
     store ?
     <div>
+    {console.log(items)}
     <h2>{store.attributes.name}</h2>
-    {store.attributes.items.map( item => (<p key={item.id}><Link className="button" to={`/items/${item.id}`}>{item.name}</Link></p>))}
-    </div> :
+    
+    {items.map( item => (<p key={item.id}><div className="itemcard"><img src={item.attributes.image_url} alt="Item" width="100" height="100"></img></div><br/><Link className="button" to={`/items/${item.id}`}>{item.attributes.name}</Link></p>))}</div>
+     :
     ""
     )
 }
