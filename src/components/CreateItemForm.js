69267973a80b2674e878ec9f6f2c6c8ca7a3c2b6
jayson_kesterson <your_email@example.com>
2020-08-16 09:54:40 -0400

    adding title to create item form

--- src/components/CreateItemForm.js
+++ src/components/CreateItemForm.js
@@ -13,13 +13,18 @@ const createItemForm = ( props ) => {
 
     const handleOnSubmit = event => {
         event.preventDefault()
-        props.createItem(props.storeId, props.price, props.name, props.description, props.condition, props.history)
+        props.createItem(props.storeId, props.price, props.imageUrl, props.name, props.description, props.condition, props.history)
     }
 
     return (
-        <form onSubmit={handleOnSubmit}>
+        <div className="login">
+            <h1>Create a New Item for your Store!</h1>
+            <form onSubmit={handleOnSubmit}>
             <label>Selling Price (USD): </label>
             <input type="number" min="1" max="10000" name="price" onChange={handleChange} value={props.price}/>
+            <br/>
+            <label>Image Url: </label>
+            <input type="text" name="imageUrl" onChange={handleChange} value={props.imageUrl}/>
             <br/>
             <label>Item Name: </label>
             <input name="name" onChange={handleChange} value={props.name}/>
@@ -32,12 +37,14 @@ const createItemForm = ( props ) => {
             <br/>
             <input type="submit" value="Create Item"/>
         </form>
+        </div>
     )};
 
     const mapStateToProps = state => {
         return{
             storeId: state.currentUserStore.id,
             price: parseInt(state.createItemForm.price),
+            imageUrl: state.createItemForm.imageUrl,
             name: state.createItemForm.name,
             description: state.createItemForm.description,
             condition: state.createItemForm.condition,
