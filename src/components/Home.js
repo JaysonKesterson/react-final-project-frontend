0c7994ca3ad4c0a99ed93e77a77164345954a601
jayson_kesterson <your_email@example.com>
2020-08-16 09:53:07 -0400

    changing home component to notloggedinnav

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Home = () => (
     <div>
-    <h2>Welcome, please <Link to="/login">Log In</Link> or <Link to="/signup">Sign Up</Link>!</h2>
+    <h2>Welcome, please <Link className="button" to="/login">Log In</Link> or <Link className="button" to="/signup">Sign Up</Link>!</h2>
     </div>
 );
 
