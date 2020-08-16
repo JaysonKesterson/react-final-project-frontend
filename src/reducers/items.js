e61dfa718c41af52bb646be89d05ec7f2c25b924
jayson_kesterson <your_email@example.com>
2020-08-16 09:47:25 -0400

    bug fix with irems reducer

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -3,11 +3,11 @@ export default (state = [], action) => {
         case "SET_ITEMS":
             return action.items
         case "ADD_ITEM":
+            console.log(action.item)
             return state.concat(action.item)
         case "EDIT_ITEM":
             return state.map(item => item.id === action.item.id ? action.item : item)
         case "DELETE_ITEM":
-            console.log("DELETE ITEM FIRE")
             return state.filter(item => item.id === action.itemId ? false : true)
             default:
             return state
